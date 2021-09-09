6c7a93dfd80a99466b035c32bcf1ac9e0a1aad8b
Gamer373 <dahopman@gmail.com>
2021-09-09 20:30:41 +0000

    Verbeteringen, Omvang Verkleind

--- script.js
+++ script.js
@@ -1,146 +1,84 @@
-
-var Bal1X = 50;
-var Bal1Y = 50;
-var Bal2X = 150;
-var Bal2Y = 50;
-var Bal3X = 250;
-var Bal3Y = 50;
-var Bal4X = 350;
-var Bal4Y = 50;
-var Bal5X = 450;
-var Bal5Y = 50;
-var SnelheidX1 = 2;
-var SnelheidY1 = 5;
-var SnelheidX2 = -5;
-var SnelheidY2 = -5;
-var SnelheidX3 = -10;
-var SnelheidY3 = -10;
-var SnelheidX4 = 5;
-var SnelheidY4 = -10;
-var SnelheidX5 = 10;
-var SnelheidY5 = 10;
+const BalX = [50, 150, 250, 350, 450];
+const BalY = [50, 50, 50, 50, 50];
+const SnelheidX = [2, -5, -10, 5, 2 ];
+const SnelheidY = [5, -5, -10, -10, 10 ];
 
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
 }
 
-
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
 function draw() {
   // stel vulkleur in
   background('blue');
+
   //bal 1
   fill(200, 100, 255);
-  ellipse(Bal1X,Bal1Y,80,80);
+  ellipse(BalX[0],BalY[0],80,80);
 
-  Bal1X = Bal1X + SnelheidX1;
-  Bal1Y = Bal1Y + SnelheidY1;
-  
-  if(Bal1X === 1240){
-    SnelheidX1 = SnelheidX1 * -1;
+  BalX[0] = BalX[0] + SnelheidX[0];
+  BalY[0] = BalY[0] + SnelheidY[0];
+  if(BalX[0] === 1240 || BalX[0] === 40){
+    SnelheidX[0] = SnelheidX[0] * -1;
   }
-  if(Bal1X === 40){
-    SnelheidX1 = SnelheidX1 * -1;
-  }
-
-  if(Bal1Y === 680){
-    SnelheidY1 = SnelheidY1 * -1;
-  }
-
-  if(Bal1Y === 40){
-    SnelheidY1 = SnelheidY1 * -1;
+  if(BalY[0] === 680 || BalY[0] === 40){
+    SnelheidY[0] = SnelheidY[0] * -1;
   }
 
   //bal 2
   fill(200, 200, 0);
-  ellipse(Bal2X,Bal2Y,80,80);
+  ellipse(BalX[1],BalY[1],80,80);
 
-  Bal2X = Bal2X + SnelheidX2;
-  Bal2Y = Bal2Y + SnelheidY2;
+  BalX[1] = BalX[1] + SnelheidX[1];
+  BalY[1] = BalY[1] + SnelheidY[1];
 
-  if(Bal2X === 1240){
-    SnelheidX2 = SnelheidX2 * -1;
+  if(BalX[1] === 1240 || BalX[1] === 40){
+    SnelheidX[1] = SnelheidX[1] * -1;
   }
-  if(Bal2X === 40){
-    SnelheidX2 = SnelheidX2 * -1;
-  }
-
-  if(Bal2Y === 680){
-    SnelheidY2 = SnelheidY2 * -1;
-  }
-
-  if(Bal2Y === 40){
-    SnelheidY2 = SnelheidY2 * -1;
+  if(BalY[1] === 680 || BalY[1] === 40){
+    SnelheidY[1] = SnelheidY[1] * -1;
   }
 
   //bal 3
   fill(200, 20, 200);
-  ellipse(Bal3X,Bal3Y,80,80);
+  ellipse(BalX[2],BalY[2],80,80);
 
-  Bal3X = Bal3X + SnelheidX3;
-  Bal3Y = Bal3Y + SnelheidY3;
+  BalX[2] = BalX[2] + SnelheidX[2];
+  BalY[2] = BalY[2] + SnelheidY[2];
 
-  if(Bal3X === 1240){
-    SnelheidX3 = SnelheidX3 * -1;
+  if(BalX[2] === 1240 || BalX[2] === 40){
+    SnelheidX[2] = SnelheidX[2] * -1;
   }
-  if(Bal3X === 40){
-    SnelheidX3 = SnelheidX3 * -1;
-  }
-
-  if(Bal3Y === 680){
-    SnelheidY3 = SnelheidY3 * -1;
-  }
-
-  if(Bal3Y === 40){
-    SnelheidY3 = SnelheidY3 * -1;
+  if(BalY[2] === 680 || BalY[2] === 40){
+    SnelheidY[2] = SnelheidY[2] * -1;
   }
 
   //bal 4 
   fill(0, 255, 0);
-  ellipse(Bal4X,Bal4Y,80,80);
+  ellipse(BalX[3],BalY[3],80,80);
 
-  Bal4X = Bal4X + SnelheidX4;
-  Bal4Y = Bal4Y + SnelheidY4;
+  BalX[3] = BalX[3] + SnelheidX[3];
+  BalY[3] = BalY[3] + SnelheidY[3];
 
-  if(Bal4X === 1240){
-    SnelheidX4 = SnelheidX4 * -1;
+  if(BalX[3] === 1240 || BalX[3] === 40){
+    SnelheidX[3] = SnelheidX[3] * -1;
   }
-  if(Bal4X === 40){
-    SnelheidX4 = SnelheidX4 * -1;
-  }
-
-  if(Bal4Y === 680){
-    SnelheidY4 = SnelheidY4 * -1;
-  }
-
-  if(Bal4Y === 40){
-    SnelheidY4 = SnelheidY4 * -1;
+  if(BalY[3] === 680 || BalY[3] === 40){
+    SnelheidY[3] = SnelheidY[3] * -1;
   }
 
   //bal 5
   fill(0, 200, 100);
-  ellipse(Bal5X,Bal5Y,80,80);
+  ellipse(BalX[4],BalY[4],80,80);
 
-  Bal5X = Bal5X + SnelheidX5;
-  Bal5Y = Bal5Y + SnelheidY5;
+  BalX[4] = BalX[4] + SnelheidX[4];
+  BalY[4] = BalY[4] + SnelheidY[4];
 
-  if(Bal5X === 1240){
-    SnelheidX5 = SnelheidX5 * -1;
+  if(BalX[4] === 1240 || BalX[4] === 40){
+    SnelheidX[4] = SnelheidX[4] * -1;
   }
-  if(Bal5X === 40){
-    SnelheidX5 = SnelheidX5 * -1;
-  }
-
-  if(Bal5Y === 680){
-    SnelheidY5 = SnelheidY5 * -1;
+  if(BalY[4] === 680 || BalY[4] === 40 ){
+    SnelheidY[4] = SnelheidY[4] * -1;
   }
 
-  if(Bal5Y === 40){
-    SnelheidY5 = SnelheidY5 * -1;
-  }
 }
